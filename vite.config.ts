ae8b0863222ba05ed807dfd3171be77b368410d9
Alaa Khalil <ak2139060@gmail.com>
2025-02-06 00:02:09 +0100

    Add PWA support with service worker and web manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/typescript_camera/'
-})
-
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'PWA-camera',
+        short_name: 'PWA-camera',
+        description: 'PWA-camera',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'web-app-manifest-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'web-app-manifest-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      srcDir: 'src',
+      filename: 'service-worker.js',
+    }),
+  ],
+  base: '/typescript_camera/', 
+});
